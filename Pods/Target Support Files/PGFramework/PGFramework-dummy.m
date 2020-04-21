0e767ffd0dfbdaaafe3dc88f96ffd3be87cadbf4
ASW-研修３ <training03@ASW-kenshuu3noMacBook-Air.local>
2020-04-21 13:40:10 +0900

    delete cache

--- Pods/Target Support Files/PGFramework/PGFramework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PGFramework : NSObject
-@end
-@implementation PodsDummy_PGFramework
-@end
--- Pods/Target Support Files/Pods-insta/Pods-insta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_insta : NSObject
-@end
-@implementation PodsDummy_Pods_insta
-@end
