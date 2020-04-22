13903fbab3548ea562e6ba07fc85ffe8d005f27c
ASW-研修３ <training03@ASW-kenshuu3noMacBook-Air.local>
2020-04-22 16:36:54 +0900

    Firebase入れる前段階

---
+++ insta/postView/postViewController.swift
@@ -0,0 +1,30 @@
+//
+//  postViewController.swift
+//  insta
+//
+//  Created by ASW-研修３ on 2020/04/22.
+//  Copyright © 2020 ASW-研修３. All rights reserved.
+//
+
+import UIKit
+
+class postViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
